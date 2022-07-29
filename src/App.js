30d0d83d1fb75f31b6f55cf76aab2c5f747caab1
Michael Salerno <msalerno1993@gmail.com>
2022-07-28 21:28:46 -0400

    fix rendering issue to page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import MovieCard from './MovieCard';
 
@@ -7,23 +7,19 @@ const API_URL = 'https://www.omdbapi.com?apikey=8a58b512'
 
 function App() {
 
-  const movie1 = {
-    Poster: "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg",
-    Type: "movie",
-    Title: "Italian Spiderman",
-    Year: "2007"
-  }
+  const [movies, setMovies] = useState([])
+  const [search, setSearch] = useState('')
+
 
   const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
 
-    const response = await fetch(`${API_URL}&s=${title}`)
-    const data = await response.json()
-
-    console.log(data.Search);
-  }
+    setMovies(data.Search);
+  };
 
   useEffect(() => {
-    searchMovies('Spiderman')
+    searchMovies()
   }, [])
 
   return (
@@ -32,18 +28,28 @@ function App() {
       <div className='search'>
         <input
           placeholder='Search for Movies'
-          value='Superman'
-          onChange={() => {}}
+          value={search}
+          onChange={(e) => {setSearch(e.target.value)}}
         />
         <button 
           className='button'
-          onClick={() => {}}
+          onClick={() => {searchMovies(search)}}
         >Search
         </button>
       </div>
-      <div className='container'>
-        <MovieCard movie1={movie1} />
-      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+
     </div>
   );
 }
